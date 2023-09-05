287cc97d7f06181f8e8eab55d0d4360aadff7117
XIVIXMMI <lethaikhoinguyen@gmail.com>
2023-09-05 20:44:45 +0700

    commit lesson 2

---
+++ 2 - Keyword, Code block/demo.js
@@ -0,0 +1,9 @@
+//Chú thích 1 dòng - single line comment
+
+/*Chú thích nhiều dòng
+Multi-line comment*/
+
+//?Keyword: là những từ khóa như if trong câu điều kiện, for trong vòng lặp, var để khai báo biến 
+//? Câu lệnh: do người dùng ghi ra và yêu cầu máy tính thực hiện kết thúc bằng " ; "
+//?Code-Block hay code snippet: là 1 tập hợp các câu lệnh được đặt trong cặp dấu {  }
+
