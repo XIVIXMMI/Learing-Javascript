e91d98e71aae92c7d0c934461db557c08d4553bd
XIVIXMMI <lethaikhoinguyen@gmail.com>
2023-09-06 20:28:07 +0700

    assigment & comparison operator

---
+++ 6 - Assignment & Comparison operator/demo.js
@@ -0,0 +1,18 @@
+//toán tử gán 
+let a = 'Hello World';
+console.log(a);
+
+/*Toán tử so sánh bao gồm: 
+    ==  : 2 giá trị bằng nhau
+    !=  : 2 giá trị khác nhau
+    >   : lớn hơn
+    <   : bé hơn
+    >=  : lớn hơn hoặc bằng
+    <=  : bé hơn hoặc bằng
+    =>tương ứng như trong toán học 
+    ngoài ra: 
+    === : 2 giá trị bằng nhau và có cùng kiểu dữ liệu
+    !== : 2 giá trị khác nhau và khác kiểu dữ liệu 
+ */
+
+    console.log(3>4);
