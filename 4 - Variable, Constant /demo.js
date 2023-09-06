4dd7290a9da1d065635969540058286d817337e0
XIVIXMMI <lethaikhoinguyen@gmail.com>
2023-09-06 17:01:36 +0700

    completed initialize variable

---
+++ 4 - Variable, Constant /demo.js
@@ -0,0 +1,27 @@
+/*
+Biến là gì ? 
+Là đại diện cho một phân vùng bộ nhớ nhất định 
+Giúp lưu trữ các giá trị cần thiết
+Việc lấy ra 1 biến cũng tương đương việc lấy giá trị của một vùng nhớ nào đó 
+
+Trong Js 1 biến chỉ được sử dụng khi nó được khai báo 
+
+Khai bằng let hoặc var 
+
+Tên biến có thể bao gồm chữ cái, ký tự số, dấu gạch dưới hoặc ký hiệu $ 
+Không được phép bắt đầu bằng ký tự số 
+
+Biến toàn cục được khởi tạo bằng var 
+
+Biến cục bộ được khởi tạo bằng let 
+
+Hằng số được khởi tạo trong chương trình 1 lần và bất biến (không thể thay đổi)
+*/ 
+
+let myVar = 5;
+
+var mySecVar = 'Hello World';
+
+const name = 'Lê Thái Khôi Nguyên'
+
+console.log(name);
---
+++ 5 - Initialize Variable & Constant using var let/demo.js
@@ -0,0 +1,18 @@
+/*
+Từ khóa var : khởi tạo biến toàn cục (phạm vi function)
+cho phép sủ dụng biến trước khi khai báo nhưng sẽ trả về undefined 
+không thể khởi tạo lại 1 biến bằng var 
+
+    eg: let t = 2;
+        var t = 3;
+        => Sẽ xảy ra lỗi 
+
+nhưng nếu đó là 1 biến var thì nó có thể ghi đè lên 
+
+    eg: var x = 2;
+        var x = 4;
+
+let thì chỉ được dùng 1 lần và không thể khai báo 1 biến khác cũng giống như biến let trước 
+
+
+ */
