6dafefba77ec5696f4991d27cd8a6d13d46dfa65
XIVIXMMI <lethaikhoinguyen@gmail.com>
2023-09-14 21:48:49 +0700

    lesson 16 completed

---
+++ 16 - Exercises on String Datatype/ex.js
@@ -0,0 +1,32 @@
+//? Nêu các dấu hiệu đặc trưng của chuỗi trong Js, cách khởi tạo chuỗi trong Js.
+/**
+ * Chuỗi là toàn bộ những ký tự chữ, số, ký tự đặt biệt 
+ * nằm bên trong cặp dấu ' ', " " hoặc ` `
+ */
+str = 'abc'
+str2 = "edf"
+str3 = `ghi`
+
+/*
+Cho biết kết quả của các phép tính sau:
+‘12’ + ‘12’ => 1212
+12 + ‘12’ => 1212
+‘12’ * 12 => 144
+‘a’ * 13 => NaN
+‘a’ + 13 => a13
+‘aa’ + ‘12’ * 3 => aa36
+ */
+
+console.log('.'.repeat(12));
+console.log('.'.repeat(3)+'+'.repeat(2)+'.'.repeat(2)+'+'.repeat(2)+'.'.repeat(3));
+console.log('.'.repeat(5)+'-'.repeat(2)+'.'.repeat(5));
+console.log('.'.repeat(4)+'_'.repeat(4)+'.'.repeat(4));
+console.log('.'.repeat(12));
+console.log(' ');
+console.log('.'.repeat(9));
+console.log('.'.repeat(4)+'+'.repeat(1)+'.'.repeat(4));
+console.log('.'.repeat(3)+'+'.repeat(3)+'.'.repeat(3));
+console.log('.'.repeat(2)+'+'.repeat(5)+'.'.repeat(2));
+console.log('.'.repeat(1)+'+'.repeat(7)+'.'.repeat(1));
+console.log('.'.repeat(4)+'+'.repeat(1)+'.'.repeat(4));
+console.log('.'.repeat(9));
